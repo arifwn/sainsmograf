f7f787755683948665c1a53373c6f3727b989e95
Arif Widi Nugroho <arif@sainsmograf.com>
2015-12-19 07:27:57 +0700

    updated site config

--- settings.py
+++ settings.py
@@ -6,5 +6,6 @@ app.config.update(
     TARGET_SERVER_PROTOCOL='http',
     SITE_NAME='Sainsmograf',
     SITE_AUTHOR='Arif Widi Nugroho',
-    BUILD_DIR='/Users/arif/projects/arifwn.github.io'
+    FREEZER_DESTINATION='/Users/arif/projects/arifwn.github.io',
+    FREEZER_DESTINATION_IGNORE=['.*']
 )
